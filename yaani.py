27fc66be684586f1ec3640436ade460f6176891b
ady <ady@cloud-temple.com>
2019-09-16 11:57:27 +0200

    Add comments in yaani.py

--- yaani.py
+++ yaani.py
@@ -195,42 +195,101 @@ class InventoryBuilder:
         return {'_meta': {'hostvars': {}}}
 
     def build_inventory(self):
-        #  If none of the list or host options is set, return an empty dict
-        if self.list_mode:
+        """Build and return the inventory dict.
+        The inventory respects the following principles :
+          - When the script is called with the single argument --list, the script must
+            output to stdout a JSON-encoded hash or dictionary containing all of the
+            groups to be managed. Each group’s value should be either a hash or dictionary
+            containing a list of each host, any child groups, and potential group variables,
+            or simply a list of hosts:
 
+            {
+                "group001": {
+                    "hosts": ["host001", "host002"],
+                    "vars": {
+                        "var1": true
+                    },
+                    "children": ["group002"]
+                },
+                "group002": {
+                    "hosts": ["host003","host004"],
+                    "vars": {
+                        "var2": 500
+                    },
+                    "children":[]
+                }
+
+            }
+
+          - If any of the elements of a group are empty they may be omitted from the output.
+          - When called with the argument --host <hostname> (where <hostname> is a host from above),
+            the script must print either an empty JSON hash/dictionary, or a hash/dictionary of
+            variables to make available to templates and playbooks. For example:
+
+            {
+                "VAR001": "VALUE",
+                "VAR002": "VALUE",
+            }
+            -------------------------------------
+          - FIXME: COMPLETE COMMENTS
+
+        Returns:
+            dict: The inventory
+        """
+        # If the list mode is specified, return a complete inventory
+        if self.list_mode:
             inventory = self._init_inventory()
 
+            # Check whether the import section exists.
+            # If it exists, iterate over each statement, else, execute default behaviour.
             if self.imports:
                 # Iterate through each import element and resolve it
                 for type_key, import_statement in self.imports.items():
                     self._execute_import(
                         import_type=type_key,
                         import_options=import_statement,
-                        inventory=inventory)
+                        inventory=inventory
+                    )
             else:
-                # By default, get all devices and handle no grouping option
-                self._execute_import(import_type='devices',
-                                     import_options={},
-                                     inventory=inventory)
+                # Execute default behaviour:
+                #   -> get all devices and handle no grouping option
+                self._execute_import(
+                    import_type='devices',
+                    import_options={},
+                    inventory=inventory
+                )
 
             return inventory
+        # If a host name is specified, return the inventory filled with only its
+        # information
         elif self.host:
             inventory = self._init_inventory()
 
-            # a host is considered to be a device. Check if devices import
+            # A host is considered to be a device. Check if devices import
             # options are set.
             device_import_options = self.imports.get('devices', {})
 
-            self._execute_import(import_type='devices',
-                                 import_options=device_import_options,
-                                 inventory=inventory)
+            self._execute_import(
+                import_type='devices',
+                import_options=device_import_options,
+                inventory=inventory
+            )
 
             return inventory['_meta']['hostvars'][self.host]
+        #  If none of the list or host options is set, return an empty dict
         else:
-            # nothing to return, nothing to generate
             return {}
 
     def _execute_import(self, import_type, import_options, inventory):
+        """Fetch requested entities in Netbox.
+
+        Args:
+            import_type (str): The type of objects to fetch from Netbox.
+            import_options (str): The complementary arguments to refine search.
+            inventory (dict): The inventory in which the information must be
+                added.
+        """
+        # Declare the dict referencing all supported import statement types
         import_types_endpoints = {
             "devices": "dcim/devices/",
             "racks": "dcim/racks/",
@@ -241,11 +300,13 @@ class InventoryBuilder:
         try:
             endpoint = import_types_endpoints[import_type]
         except KeyError:
+            # If the specified import type is not supported, end program execution.
             sys.exit(
                 "Error: %s type"
                 " is not supported under import section." % import_type
             )
 
+        # Fetch the list of entities from Netbox
         netbox_hosts_list = self._get_elements_list(
             self.api_url + endpoint,
             api_token=self.api_token,
@@ -253,127 +314,47 @@ class InventoryBuilder:
             specific_host=self.host
         )
 
+        # If the netbox hosts list fetching was successful, add the elements to
+        # the inventory.
         if netbox_hosts_list:
             for host in netbox_hosts_list:
+                # Compute an id for the given host data
                 element_name = self._get_identifier(host, import_type)
+                # Add the element to the propper group(s)
                 self._add_element_to_inventory(
                     element_name, host, inventory, import_type,
                     import_options.get('group_by', None),
                     import_options.get('group_prefix', None)
                 )
+                # Load the host vars in the inventory
                 self._load_element_vars(
-                    element_name, host, inventory, import_options.get('host_vars', None)
+                    element_name, host,
+                    inventory,
+                    import_options.get('host_vars', None)
                 )
-
-
-    def _get_identifier(self, host, obj_type):
-        r = host.get('name')
-        if r is None or r == "":
-            r = "%s_%s" % (obj_type, host.get('id'))
-        return r
-
-    def _ensure_group_exists(self, group_name, inventory):
-        inventory.setdefault(group_name, {})
-        inventory[group_name].setdefault('hosts', [])
-        return inventory
-
-    def _load_element_vars(self, element_name, host, inventory, host_vars=None):
-        # If there is no required host var to load, end here.
-        if host_vars:
-            host_data = {}
-            # Iterate over every key value pairs in host_vars required in
-            # config file
-            for key, value in host_vars.items():
-                # Set the name of the key
-                if value == 'ALL':
-                    host_data[key] = host
-                else:
-                    try:
-                        host_data[key] = self._dig_value(
-                            key_path=value, data=host)
-                    except KeyError:
-                        sys.exit("Error: Key '%s' not found" % value)
-
-            # Add the loaded variables in the inventory under the proper
-            # section (name of the host)
-            inventory['_meta']['hostvars'].update(
-                {element_name: host_data}
-            )
-
-    def _dig_value(self, key_path, data):
-        key_path_list = key_path.split(".")
-
-        pointer = data
-        for key in key_path_list:
-            try:
-                if key in list(pointer.keys()):
-                    pointer = pointer[key]
-                else:
-                    sys.exit(
-                        "Error: The key solving failed "
-                        "in key_path '%s'" % (key_path)
-                    )
-            except AttributeError:
-                return None
-
-        return pointer
-
-    def _add_element_to_inventory(self, element_name, host, inventory, obj_type,
-                                  group_by=None, group_prefix=None):
-        if group_by:
-            for group in group_by:
-                if self._dig_value(key_path=group, data=host) is not None:
-                    if group == 'tags':
-                        for tag in host.get(group):
-                            group_name = tag
-                            if group_prefix is not None:
-                                group_name = group_prefix + group_name
-                            self._add_element_to_group(
-                                element_name=element_name,
-                                group_name=group_name,
-                                inventory=inventory)
-                    else:
-                        group_name = self._dig_value(key_path=group, data=host)
-                        if group_prefix:
-                            group_name = group_prefix + group_name
-                        self._add_element_to_group(
-                            element_name=element_name, group_name=group_name,
-                            inventory=inventory
-                        )
-        # Anyway, add the host to its main type group: devices, racks etc. and to the
-        # group 'all'
-        self._add_element_to_group(
-            element_name=element_name, group_name=obj_type, inventory=inventory
-        )
-        self._add_element_to_group(
-            element_name=element_name, group_name='all', inventory=inventory
-        )
-
-    def _add_element_to_group(self, element_name, group_name, inventory):
-        self._ensure_group_exists(group_name=group_name, inventory=inventory)
-        if element_name not in inventory.get(group_name).get('hosts'):
-            inventory[group_name]['hosts'].append(element_name)
 
     def _get_elements_list(self, api_url, api_token=None,
                            cfilter=None, specific_host=None):
-        """Retrieves hosts list from netbox API.
+        """Retrieves a list of element from netbox API.
 
         Returns:
-                A list of all hosts from netbox API.
+                A list of all elements from netbox API.
         """
 
         if not api_url:
             sys.exit("Missing API URL in configuration file")
 
+        # cURL options
         api_url_headers = {}
         api_url_params = {}
 
+        # If a token is specified, take it into account.
         if api_token:
             # load token in headers
             api_url_headers.update({"Authorization": "Token %s" % api_token})
 
+        # If a host name was specified, narrow search to one specific host.
         if specific_host:
-            # narrow search to one specific host
             api_url_params.update({"name": specific_host})
 
         # Resolve the filter
@@ -405,6 +386,154 @@ class InventoryBuilder:
         # Get hosts list.
         return api_output_data["results"]
 
+    def _add_element_to_inventory(self, element_name, host, inventory, obj_type,
+                                  group_by=None, group_prefix=None):
+        """Insert the given element in the propper groups.
+
+        Args:
+            element_name (str): The name of the element
+            host (dict): The actual data of the element
+            inventory (dict): The inventory in which the element must be inserted.
+            obj_type (str): The type of the element
+            group_by (list, optional): The list of group ot create and to add
+                the element to.
+            group_prefix (str, optional): An optional prefix to add in front of
+                group names that will be created.
+        """
+        # If the group_by option is specified, insert the element in the propper
+        # groups.
+        if group_by:
+            # Iterate over every groups
+            for group in group_by:
+                # Check that the specified group points towards an actual value
+                if self._resolve_expression(key_path=group, data=host) is not None:
+                    # The 'tags' field is a list, a second iteration must be
+                    # performed at a deeper level
+                    if group == 'tags':
+                        # Iterate over every tag
+                        for tag in host.get(group):
+                            group_name = tag
+                            # Add the optional prefix
+                            if group_prefix is not None:
+                                group_name = group_prefix + group_name
+                            # Insert the element in the propper group
+                            self._add_element_to_group(
+                                element_name=element_name,
+                                group_name=group_name,
+                                inventory=inventory
+                            )
+                    else:
+                        # Check that the specified group points towards an actual
+                        # value
+                        group_name = self._resolve_expression(key_path=group, data=host)
+                        # Add the optional prefix
+                        if group_prefix:
+                            group_name = group_prefix + group_name
+                        # Insert the element in the propper group
+                        self._add_element_to_group(
+                            element_name=element_name,
+                            group_name=group_name,
+                            inventory=inventory
+                        )
+        # Anyway, add the host to its main type group ( evices, racks, etc.) and to the
+        # group 'all'
+        self._add_element_to_group(
+            element_name=element_name, group_name=obj_type, inventory=inventory
+        )
+        self._add_element_to_group(
+            element_name=element_name, group_name='all', inventory=inventory
+        )
+
+    def _add_element_to_group(self, element_name, group_name, inventory):
+        self._initialize_group(group_name=group_name, inventory=inventory)
+        if element_name not in inventory.get(group_name).get('hosts'):
+            inventory[group_name]['hosts'].append(element_name)
+
+    def _get_identifier(self, host, obj_type):
+        """Return an identifier for the given host.
+
+        Args:
+            host (str): The name of the host
+            obj_type (str): The type of the object
+
+        Returns:
+            str: The computed id
+        """
+        # Get the 'name' field value of the specified host
+        r = host.get('name')
+        # If the 'name' field is empty, compute the id :
+        #   <object type>_<id in netbox db>
+        if r is None or r == "":
+            r = "%s_%s" % (obj_type, host.get('id'))
+        return r
+
+    def _initialize_group(self, group_name, inventory):
+        """
+        Args:
+            group_name (str): The group to be initialized
+            inventory (dict): The inventory including the group
+
+        Returns:
+            The updated inventory
+        """
+        # Initialize the group in the inventory
+        inventory.setdefault(group_name, {})
+        # Initialize the host field of the group
+        inventory[group_name].setdefault('hosts', [])
+        return inventory
+
+    def _load_element_vars(self, element_name, host, inventory, host_vars=None):
+        # If there is no required host var to load, end here.
+        if host_vars:
+            host_data = {}
+            # Iterate over every key value pairs in host_vars required in
+            # config file
+            for key, value in host_vars.items():
+                # Set the name of the key
+                if value == 'ALL':
+                    host_data[key] = host
+                else:
+                    try:
+                        host_data[key] = self._resolve_expression(
+                            key_path=value,
+                            data=host
+                        )
+                    except KeyError:
+                        sys.exit("Error: Key '%s' not found" % value)
+
+            # Add the loaded variables in the inventory under the proper
+            # section (name of the host)
+            inventory['_meta']['hostvars'].update(
+                {element_name: host_data}
+            )
+
+    def _resolve_expression(self, key_path, data):
+        """Resolve the given key path to a value.
+
+        Args:
+            key_path (str): The path toward the key
+            data (dict): The actual host data dict that holds
+                the target value.
+
+        Returns:
+            The target value
+        """
+        key_path_list = key_path.split(".")
+
+        pointer = data
+        for key in key_path_list:
+            try:
+                if key in list(pointer.keys()):
+                    pointer = pointer[key]
+                else:
+                    sys.exit(
+                        "Error: The key solving failed "
+                        "in key_path '%s'" % (key_path)
+                    )
+            except AttributeError:
+                return None
+
+        return pointer
 
 def dump_json_inventory(inventory):
     """Dumps the given inventory in json
@@ -416,12 +545,15 @@ def dump_json_inventory(inventory):
 
 
 def main():
+    # Parse cli args
     args = parse_cli_args(sys.argv[1:])
+    # Parse the configuration file
     configuration = load_config_file(args.config_file)
 
+    # Build the inventory
     builder = InventoryBuilder(args, configuration)
+    # Print the JSON formatted inventory dict
     dump_json_inventory(builder.build_inventory())
-
 
 if __name__ == '__main__':
     main()
