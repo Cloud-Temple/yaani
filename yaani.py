b76eaa7405290852dd79d52cf6932c2d6d0c071b
ady <ady@cloud-temple.com>
2019-09-30 17:57:25 +0200

    Implement sub imports feature

--- yaani.py
+++ yaani.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
 from __future__ import absolute_import
 
+from functools import reduce
+import logging
 import argparse
 import sys
 import os
@@ -23,7 +24,7 @@ DEFAULT_ENV_CONFIG_FILE = "NETBOX_CONFIG_FILE"
 
 
 class Namespace:
-    def __init__(self, name, key, vars_dict = {}):
+    def __init__(self, name, key, vars_dict={}):
         self.vars = vars_dict.copy()
         self.name = name
         self.key = key
@@ -51,27 +52,84 @@ class Namespace:
 
 
 class StackTransformer(Transformer):
-    def __init__(self, stack_string, api_connector, import_namespace, sub_import_namespace):
+    def __init__(self, vars_definition, api_connector, import_namespace,
+                 sub_import_namespace):
         self.api_connector = api_connector
-        self.stack_string = stack_string
+        self.vars_definition = vars_definition
         self.import_namespace = import_namespace
         self.sub_import_namespace = sub_import_namespace
 
-    def custom(self):
-        pass
+    def _import_var(self, parent_namespace, loading_var):
+        # Access var configuration
+        try:
+            var_configuration = self.vars_definition[loading_var]
+        except KeyError:
+            sys.exit(
+                "Bad key %s in sub-import section. Variable not defined in "
+                "sub_import.vars section." % loading_var
+            )
+
+        # Access netbox location of wanted object
+        app = getattr(self.api_connector, var_configuration['application'])
+        endpoint = getattr(app, var_configuration['type'])
+
+        # Resolve filter args from "device_id": id to "device_id": 123
+        filter_args = {}
+        for k, v in var_configuration['filter'].items():
+            try:
+                filter_args[k] = parent_namespace[v]
+            except KeyError:
+                sys.exit(
+                    "The key given as a filter value '%s' does not exist." % v
+                )
+
+        # fetch sub elements from netbox
+        elements = endpoint.filter(**filter_args)
+
+        # Set the name of the key that will be used for index
+        index_key_name = var_configuration['index']
+
+        ret = {}
+        for e in elements:
+            # Resolve the actual index value
+            key = getattr(e, index_key_name)
+            if key in list(ret.keys()):
+                sys.exit(
+                    "The key '%s', specified as an index key, is resolved to "
+                    "non unique values." % key
+                )
+            ret[key] = dict(e)
+
+        return ret
 
     def stack(self, n):
-        pass
+        return n[0]
 
     def nested_path(self, n):
-        pass
+        sub_pointer = [self.sub_import_namespace]
+        parent_ns = self.import_namespace
+
+        for path in n:
+            l = []
+            for v in sub_pointer:
+                if parent_ns is None:
+                    parent_ns = v
+                v[str(path)] = self._import_var(
+                    parent_namespace=parent_ns,
+                    loading_var=str(path)
+                )
+                l += list(v[str(path)].values())
+            parent_ns = None
+            sub_pointer = l
+
+        return self.sub_import_namespace
 
 
 class KeyPathTransformer(Transformer):
     """The transformer to apply to tree of type System"""
-    def __init__(self, build_ns = Namespace("build", "b"),
-                 import_ns = Namespace("import", "i"),
-                 sub_import_ns = Namespace("sub-import", "s")):
+    def __init__(self, build_ns=Namespace("build", "b"),
+                 import_ns=Namespace("import", "i"),
+                 sub_import_ns=Namespace("sub-import", "s")):
         self.namespaces = {}
         self.namespaces[build_ns.get_key()] = build_ns
         self.namespaces[import_ns.get_key()] = import_ns
@@ -143,7 +201,6 @@ class InventoryBuilder:
         list_mode (bool): The value of --list option
         parser (Lark): The parser used to parse custom expressions.
     """
-
     def __init__(self, script_args, script_config):
         # Script args
         self.config_file = script_args.config_file
@@ -191,50 +248,15 @@ class InventoryBuilder:
             key_path_grammar, start="expr", parser='lalr'
         )
 
+        self.stack_parser = Lark(
+            stack_grammar, start="stack",
+        )
+
     def _init_inventory(self):
         return {'_meta': {'hostvars': {}}}
 
     def build_inventory(self):
         """Build and return the inventory dict.
-        The inventory respects the following principles :
-          - When the script is called with the single argument --list, the
-            script must output to stdout a JSON-encoded hash or dictionary
-            containing all of the groups to be managed. Each group’s value
-            should be either a hash or dictionary containing a list of each
-            host, any child groups, and potential group variables,
-            or simply a list of hosts:
-
-            {
-                "group001": {
-                    "hosts": ["host001", "host002"],
-                    "vars": {
-                        "var1": true
-                    },
-                    "children": ["group002"]
-                },
-                "group002": {
-                    "hosts": ["host003","host004"],
-                    "vars": {
-                        "var2": 500
-                    },
-                    "children":[]
-                }
-
-            }
-
-          - If any of the elements of a group are empty they may be omitted
-            from the output.
-          - When called with the argument --host <hostname> (where <hostname>
-            is a host from above), the script must print either an empty JSON
-            hash/dictionary, or a hash/dictionary of variables to make
-            available to templates and playbooks. For example:
-
-            {
-                "VAR001": "VALUE",
-                "VAR002": "VALUE",
-            }
-            -------------------------------------
-          - FIXME: COMPLETE COMMENTS
 
         Returns:
             dict: The inventory
@@ -271,8 +293,8 @@ class InventoryBuilder:
             return inventory
         else:
             # The host mode is on:
-            #   If a host name is specified, return the inventory filled with only
-            #   its information
+            #   If a host name is specified, return the inventory filled with
+            #   only its information.
             #   A host is considered to be a device. Check if devices import
             #   options are set.
             device_import_options = (
@@ -361,20 +383,23 @@ class InventoryBuilder:
                 {element_name: dict(build_ns)}
             )
 
-    def _execute_sub_import(self, sub_import, import_namespace, sub_import_namespace):
+    def _execute_sub_import(self, sub_import, import_namespace,
+                            sub_import_namespace):
         # Extract stack string
         stack_string = sub_import['stack']
 
         # Extract vars definition
-        vars_def = sub_import['vars']
+        vars_definition = {}
+        for i in sub_import['vars']:
+            vars_definition.update(i)
 
         t = StackTransformer(
             api_connector=self.nb,
-            stack_string=stack_string,
+            vars_definition=vars_definition,
             import_namespace=import_namespace,
             sub_import_namespace=sub_import_namespace
         )
-        pass
+        return t.transform(self.stack_parser.parse(stack_string))
 
     def _add_element_to_inventory(self, element_index, host_dict, inventory,
                                   obj_type, group_by=None, group_prefix=None,
@@ -397,7 +422,7 @@ class InventoryBuilder:
         import_namespace = Namespace("import", "i", host_dict)
         sub_import_namespace = Namespace("sub-import", "s")
 
-        # FIXME : Handle sub imports
+        # Handle sub imports
         self._execute_sub_import(
             sub_import,
             import_namespace,
@@ -415,7 +440,8 @@ class InventoryBuilder:
         # Add the host to its main type group (devices, racks, etc.)
         # and to the group 'all'
         self._add_element_to_group(
-            element_name=element_index, group_name=obj_type, inventory=inventory
+            element_name=element_index, group_name=obj_type,
+            inventory=inventory
         )
         self._add_element_to_group(
             element_name=element_index, group_name='all', inventory=inventory
@@ -443,7 +469,8 @@ class InventoryBuilder:
                             inventory=inventory
                         )
                 else:
-                    # Check that the specified group points towards an actual value
+                    # Check that the specified group points towards an
+                    # actual value
                     group_name = self._resolve_expression(
                         key_path=group,
                         build_ns=build_namespace,
@@ -526,7 +553,8 @@ class InventoryBuilder:
         inventory[group_name].setdefault('hosts', [])
         return inventory
 
-    def _resolve_expression(self, key_path, build_ns, import_ns, sub_import_ns):
+    def _resolve_expression(self, key_path,
+                            build_ns, import_ns, sub_import_ns):
         """Resolve the given key path to a value.
 
         Args:
@@ -586,6 +614,48 @@ def validate_configuration(configuration):
     Args:
             configuration (dict): The parsed configuration
     """
+    sub_import_def = {
+        "type": "object",
+        "required": ["stack", "vars"],
+        "properties": {
+            "stack": {
+                "type": "string"
+            },
+            "vars": {
+                "type": "array",
+                "minItems": 1,
+                "items": {
+                    "type": "object",
+                    "maxProperties": 1,
+                    "minProperties": 1,
+                    "patternProperties": {
+                        "\\w+": {
+                            "type": "object",
+                            "properties": {
+                                "application": {
+                                    "type": "string"
+                                },
+                                "type": {
+                                    "type": "string"
+                                },
+                                "index": {
+                                    "type": "string"
+                                },
+                                "filter": {
+                                    "type": "object",
+                                    "patternProperties": {
+                                        "\\w+": {
+                                            "type": "string"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     config_schema = {
         "$schema": "http://json-schema.org/draft-07/schema#",
@@ -666,48 +736,7 @@ def validate_configuration(configuration):
                                                     "type": "string"
                                                 }
                                             },
-                                            "sub_import": {
-                                                "type": "object",
-                                                "required": ["stack","vars"],
-                                                "properties": {
-                                                    "stack": {
-                                                        "type": "string"
-                                                    },
-                                                    "vars": {
-                                                        "type": "array",
-                                                        "minItems": 1,
-                                                        "items": {
-                                                            "type": "object",
-                                                            "maxProperties": 1,
-                                                            "minProperties": 1,
-                                                            "patternProperties": {
-                                                                "\\w+": {
-                                                                    "type": "object",
-                                                                    "properties": {
-                                                                        "application": {
-                                                                            "type": "string"
-                                                                        },
-                                                                        "type": {
-                                                                            "type": "string"
-                                                                        },
-                                                                        "index": {
-                                                                            "type": "string"
-                                                                        },
-                                                                        "filter": {
-                                                                            "type": "object",
-                                                                            "patternProperties": {
-                                                                                "\\w+": {
-                                                                                    "type": "string"
-                                                                                }
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            },
+                                            "sub_import": sub_import_def,
                                             "group_prefix": {
                                                 "type": "string",
                                             },
