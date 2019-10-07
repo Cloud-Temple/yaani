ffb6c5ae9a1000e79817ba36873e33ff38bfb900
ady <ady@cloud-temple.com>
2019-10-07 12:13:55 +0200

    Implement tests for inventory builder methods:  - get_identifier  - _initialize_group

--- tests/test_inventory_builder.py
+++ tests/test_inventory_builder.py
@@ -1,7 +1,8 @@
 import pytest
 
-from yaani import InventoryBuilder
-from yaani import Transformer
+from yaani import (
+    InventoryBuilder
+)
 
 
 @pytest.fixture
@@ -22,11 +23,13 @@ def config():
     conf = {
         "netbox": {
             "api": {
-                "api_url": "url test"
+                "url": "url test"
             },
             "import": {
                 "devices": {
-                    "filter": "test"
+                    "filters": {
+                        "role_id": 20
+                    }
                 }
             }
         }
@@ -39,3 +42,87 @@ def config():
 def inv_builder(cli_args, config):
     return InventoryBuilder(cli_args, config)
 
+
+@pytest.mark.parametrize("group,inventory,expected", [
+    (  # non existing group
+        "test_group",
+        {},
+        {
+            "test_group": {
+                "hosts": []
+            }
+        }
+    ),
+    (  # existing group with no hosts
+        "test_group",
+        {
+            "test_group": {
+                "test_key": "1"
+            }
+        },
+        {
+            "test_group": {
+                "test_key": "1",
+                "hosts": []
+            }
+        }
+    ),
+    (  # existing group with hosts
+        "test_group",
+        {
+            "test_group": {
+                "hosts": [
+                    1,
+                    2
+                ]
+            }
+        },
+        {
+            "test_group": {
+                "hosts": [
+                    1,
+                    2
+                ]
+            }
+        }
+    ),
+])
+def test_initialize_group(inv_builder, group, inventory, expected):
+    """Test the _initialize_group method properly works"""
+    assert inv_builder._initialize_group(group, inventory) == expected
+
+
+
+@pytest.mark.parametrize("host,obj_type,expected", [
+    (  # present name
+        {
+            "id": 1,
+            "name": "test_name"
+        },
+        "device",
+        "test_name"
+    ),
+    (  # absent name, present id
+        {
+            "id": 1,
+        },
+        "device",
+        "device_1"
+    ),
+])
+def test_get_identifier_ok(inv_builder, host, obj_type, expected):
+    """Test the get_identifier method properly works"""
+    assert inv_builder._get_identifier(host, obj_type) == expected
+
+
+@pytest.mark.parametrize("host,obj_type", [
+    (  # absent name, present id
+        {},
+        "device"
+    ),
+])
+def test_get_identifier_ko(inv_builder, host, obj_type):
+    """Test the get_identifier method raises an error when it should"""
+    with pytest.raises(SystemExit) as err:
+        inv_builder._get_identifier(host, obj_type)
+    assert "The id key is not present" in str(err.value)
