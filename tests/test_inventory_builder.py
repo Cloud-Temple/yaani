4dcb8f33ffaa18af242e09d630b408c2c93de71c
Antoine Delannoy <antoine.delannoy@cloud-temple.com>
2019-10-07 12:19:55 +0200

    Revert "Add tests"

--- tests/test_inventory_builder.py
+++ tests/test_inventory_builder.py
@@ -1,8 +1,7 @@
 import pytest
 
-from yaani import (
-    InventoryBuilder
-)
+from yaani import InventoryBuilder
+from yaani import Transformer
 
 
 @pytest.fixture
@@ -23,13 +22,11 @@ def config():
     conf = {
         "netbox": {
             "api": {
-                "url": "url test"
+                "api_url": "url test"
             },
             "import": {
                 "devices": {
-                    "filters": {
-                        "role_id": 20
-                    }
+                    "filter": "test"
                 }
             }
         }
@@ -43,86 +40,37 @@ def inv_builder(cli_args, config):
     return InventoryBuilder(cli_args, config)
 
 
-@pytest.mark.parametrize("group,inventory,expected", [
-    (  # non existing group
-        "test_group",
-        {},
-        {
-            "test_group": {
-                "hosts": []
-            }
+@pytest.fixture
+def test_data():
+    return {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": None,
+        "e": {
+            "e_a": "test value"
         }
-    ),
-    (  # existing group with no hosts
-        "test_group",
-        {
-            "test_group": {
-                "test_key": "1"
-            }
-        },
-        {
-            "test_group": {
-                "test_key": "1",
-                "hosts": []
-            }
-        }
-    ),
-    (  # existing group with hosts
-        "test_group",
-        {
-            "test_group": {
-                "hosts": [
-                    1,
-                    2
-                ]
-            }
-        },
-        {
-            "test_group": {
-                "hosts": [
-                    1,
-                    2
-                ]
-            }
-        }
-    ),
+    }
+
+
+@pytest.mark.parametrize("arg, exp", [
+    ("a", 1),  # regular key
+    ("b", 2),  # regular key
+    ("c", 3),  # regular key
+    ("d", None),  # regular key with None value
+    ("e", {"e_a": "test value"}),  # regular key with dict value
+    ("e.e_a", "test value"),  # key in a dict
+    ("a | default_key(b)", 1),  # default_key test on non null value
+    ("d | default_key(b)", 2),  # default_key test on null value
+    ("e.e_a | sub(\"value\",\"\")", "test "),  # sub on non null value
+    # sub on non null value with optional occurence count
+    ("e.e_a | sub(\"e\",\"\", 1)", "tst value"),
+    # sub on non null value with optional occurence count
+    ("e.e_a | sub(\"e$\",\"\", 1)", "test valu"),
+    ("d | sub(\"value\",\"\")", None),  # sub non null value
 ])
-def test_initialize_group(inv_builder, group, inventory, expected):
-    """Test the _initialize_group method properly works"""
-    assert inv_builder._initialize_group(group, inventory) == expected
+def test_expr_reso_grammar_ok(inv_builder, test_data, arg, exp):
+    p = inv_builder.parser
 
-
-
-@pytest.mark.parametrize("host,obj_type,expected", [
-    (  # present name
-        {
-            "id": 1,
-            "name": "test_name"
-        },
-        "device",
-        "test_name"
-    ),
-    (  # absent name, present id
-        {
-            "id": 1,
-        },
-        "device",
-        "device_1"
-    ),
-])
-def test_get_identifier_ok(inv_builder, host, obj_type, expected):
-    """Test the get_identifier method properly works"""
-    assert inv_builder._get_identifier(host, obj_type) == expected
-
-
-@pytest.mark.parametrize("host,obj_type", [
-    (  # absent name, present id
-        {},
-        "device"
-    ),
-])
-def test_get_identifier_ko(inv_builder, host, obj_type):
-    """Test the get_identifier method raises an error when it should"""
-    with pytest.raises(SystemExit) as err:
-        inv_builder._get_identifier(host, obj_type)
-    assert "The id key is not present" in str(err.value)
+    t = Transformer(data=test_data)
+    assert t.transform(p.parse(arg)) == exp
