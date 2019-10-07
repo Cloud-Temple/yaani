9e3e2138f7858c3084628c4b533a32116e2af04b
ady <ady@cloud-temple.com>
2019-10-07 14:21:20 +0200

    Add tests for expression resolution of inventory builder

--- tests/test_inventory_builder.py
+++ tests/test_inventory_builder.py
@@ -18,6 +18,54 @@ def cli_args():
 
 
 @pytest.fixture
+def namespaces():
+    return {
+        "build": {
+            "b1": "bv1",
+            "b2": "bv2",
+            "b3": "bv3",
+            "b4": [
+                "a",
+                "b",
+                "c"
+            ],
+            "b5": {
+                "bk1": "bkv1",
+                "bk2": "bkv2",
+            }
+        },
+        "import": {
+            "i1": "iv1",
+            "i2": "iv2",
+            "i3": "iv3",
+            "i4": [
+                "a",
+                "b",
+                "c"
+            ],
+            "i5": {
+                "ik1": "ikv1",
+                "ik2": "ikv2",
+            }
+        },
+        "sub-import": {
+            "s1": "sv1",
+            "s2": "sv2",
+            "s3": "sv3",
+            "s4": [
+                "a",
+                "b",
+                "c"
+            ],
+            "s5": {
+                "sk1": "skv1",
+                "sk2": "skv2",
+            }
+        }
+    }
+
+
+@pytest.fixture
 def config():
     """Return simple argument set coherent with InventoryBuilder"""
     conf = {
@@ -194,3 +242,140 @@ def test_add_element_to_group(inv_builder, element_name,
     assert inv_builder._add_element_to_group(
         element_name, group_name, inventory
     ) == expected
+
+
+@pytest.mark.parametrize("key_path,expected", [
+    (  # single item of import section (default section)
+        ".i1",
+        "iv1"
+    ),
+    (  # single item of import section (unknown namespace)
+        "a#.i1",
+        "iv1"
+    ),
+    (  # single item of import section
+        "i#.i1",
+        "iv1"
+    ),
+    (  # single item of import section in list
+        "il#.i1",
+        ["iv1"]
+    ),
+    (  # list items of import section
+        "i#.i4",
+        ["a", "b", "c"]
+    ),
+    (  # unfolded list of import section
+        "il#.i4[]",
+        ["a", "b", "c"]
+    ),
+    (  # unfolded list of import section first element
+        "i#.i4[]",
+        "a"
+    ),
+    (  # dict of import section
+        "i#.i5",
+        {
+            "ik1": "ikv1",
+            "ik2": "ikv2"
+        }
+    ),
+    (  # dict of import section in a list
+        "il#.i5",
+        [{
+            "ik1": "ikv1",
+            "ik2": "ikv2"
+        }]
+    ),
+    (  # unfolded dict of import section as a list
+        "il#.i5[]",
+        [
+            "ikv1",
+            "ikv2"
+        ]
+    ),
+    (  # single item of build section
+        "b#.b1",
+        "bv1"
+    ),
+    (  # single item of build section in list
+        "bl#.b1",
+        ["bv1"]
+    ),
+    (  # list items of build section
+        "b#.b4",
+        ["a", "b", "c"]
+    ),
+    (  # unfolded list of build section
+        "bl#.b4[]",
+        ["a", "b", "c"]
+    ),
+    (  # unfolded list of build section first element
+        "b#.b4[]",
+        "a"
+    ),
+    (  # dict of build section
+        "b#.b5",
+        {
+            "bk1": "bkv1",
+            "bk2": "bkv2"
+        }
+    ),
+    (  # dict of build section in a list
+        "bl#.b5",
+        [{
+            "bk1": "bkv1",
+            "bk2": "bkv2"
+        }]
+    ),
+    (  # # unfolded dict of build section as a list
+        "bl#.b5[]",
+        [
+            "bkv1",
+            "bkv2"
+        ]
+    ),
+    (  # single item of sub-import section
+        "s#.s1",
+        "sv1"
+    ),
+    (  # single item of sub-import section in list
+        "sl#.s1",
+        ["sv1"]
+    ),
+    (  # list items of sub-import section
+        "s#.s4",
+        ["a", "b", "c"]
+    ),
+    (  # unfolded list of sub-import section
+        "sl#.s4[]",
+        ["a", "b", "c"]
+    ),
+    (  # unfolded list of sub-import section first element
+        "s#.s4[]",
+        "a"
+    ),
+    (  # dict of sub-import section
+        "s#.s5",
+        {
+            "sk1": "skv1",
+            "sk2": "skv2"
+        }
+    ),
+    (  # dict of sub-import section in a list
+        "sl#.s5",
+        [{
+            "sk1": "skv1",
+            "sk2": "skv2"
+        }]
+    ),
+    (  #  unfolded dict of sub-import section as a list
+        "sl#.s5[]",
+        [
+            "skv1",
+            "skv2"
+        ]
+    ),
+])
+def test_resolve_expression(inv_builder, namespaces, key_path, expected):
+    assert inv_builder._resolve_expression(key_path, namespaces) == expected
