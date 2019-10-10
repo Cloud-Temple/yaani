31a847a50eb9cdd5e8871910aa7ce3d047135a6a
ady <ady@cloud-temple.com>
2019-10-10 12:08:28 +0200

    Package yaani

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="yaani",
+    name="yaani-ady",
     version="1.0.0",
     author="Antoine DELANNOY",
     author_email="antoine.j.m.delannoy@gmail.com",
     description="An Ansible dynamic inventory plugin",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Cloud-Temple/yaani.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: GNU GPL"
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
 )
